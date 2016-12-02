d54d4d4ffd3780cd03f1f293b460c1647df9468a
Tommy-Lim <tlim24@gmail.com>
2016-12-01 17:18:19 -0800

    disabled cutting when not counting down

--- bomb.js
+++ bomb.js
@@ -10,14 +10,40 @@ var interval;
 var wireArray=[0,0,0,0,0];
 var userArray=[0,0,0,0,0];
 var startTime;
+var status = 0;
 
-wire1.addEventListener('click',function(){wire1.className="wire cut-blue"; checkCut(wire1.getAttribute("value"));});
-wire2.addEventListener('click',function(){wire2.className="wire cut-red"; return checkCut(wire2.getAttribute("value"));});
-wire3.addEventListener('click',function(){wire3.className="wire cut-yellow"; return checkCut(wire3.getAttribute("value"));});
-wire4.addEventListener('click',function(){wire4.className="wire cut-white"; return checkCut(wire4.getAttribute("value"));});
-wire5.addEventListener('click',function(){wire5.className="wire cut-green"; return checkCut(wire5.getAttribute("value"));});
-//
-// document.getElementById('container-timer').textContent = "Press Start";
+wire1.addEventListener('click',function(){
+  console.log("hello");
+  if(status==1){
+    console.log("hello2");
+    wire1.className="wire cut-blue";
+    return checkCut(wire1.getAttribute("value"));
+  } else{}
+});
+wire2.addEventListener('click',function(){
+  if(status==1){
+    wire2.className="wire cut-red";
+    return checkCut(wire2.getAttribute("value"));
+  } else{}
+});
+wire3.addEventListener('click',function(){
+  if(status==1){
+    wire3.className="wire cut-yellow";
+    return checkCut(wire3.getAttribute("value"));
+  } else{}
+});
+wire4.addEventListener('click',function(){
+  if(status==1){
+    wire4.className="wire cut-white";
+    return checkCut(wire4.getAttribute("value"));
+  } else{}
+});
+wire5.addEventListener('click',function(){
+  if(status==1){
+    wire5.className="wire cut-green";
+    return checkCut(wire5.getAttribute("value"));
+  }else{}
+});
 
 var setWires = function(){
   for(i=0; i<5; i++){
@@ -46,7 +72,6 @@ var countDown = function(){
   document.getElementById('container-timer').textContent = time;
   time--;
   var percent = Math.floor((1-(time/startTime))*100)+"%";
-  console.log(percent);
   document.getElementById('tracker').style.width=percent;
   if(time<=0){
     document.getElementById('container-timer').textContent = time;
@@ -59,13 +84,16 @@ var countDown = function(){
 
 var blowup = function(){
   clearInterval(interval);
-  document.getElementById('container-timer').textContent = "You failed!";
+  document.getElementById('container-timer').textContent = "Fail! - "+time+"s left";
   document.getElementsByTagName('body')[0].className = "exploded";
+  status = 0;
 };
 
 var win = function(){
   clearInterval(interval);
   document.getElementById('container-timer').textContent = "You win!";
+  document.getElementById('container-timer').style.color="rgb(128, 251, 62)";
+  status = 0;
 };
 
 var reset = function(){
@@ -74,8 +102,11 @@ var reset = function(){
   wire3.className="wire yellow";
   wire4.className="wire white";
   wire5.className="wire green";
+  document.getElementById('tracker').style.width="0%";
+  document.getElementById('tracker').style.color="red";
   setWires();
   checkCut();
+  status = 1;
   document.getElementById('body').className="unexploded";
   clearInterval(interval);
   time = document.getElementById('seconds').value;
